d6b4ee95aa94247d2f25c272e1893cf3c958f1f9
XenHunt <95288387+XenHunt@users.noreply.github.com>
2024-09-30 13:13:07 +0300

    Changes: replace pg with bun as pg is only on maintenance and will not get future updates.

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,26 +1,27 @@
 package db
 
 import (
-	"log"
+	"database/sql"
+	"fmt"
 
 	"github.com/XenHunt/go-test-project/internal/config"
-	"github.com/go-pg/pg/v10"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
 )
 
 type RTokens struct {
-	Token string
+	bun.BaseModel `bun:"table:refresh_tokens,alias:rtokens"`
+	Token         string
 }
 
-func MakeConection(dbConfig config.DataBaseConfig) *pg.DB {
-	opt, err := pg.ParseURL("postgres://" + dbConfig.User + ":" + dbConfig.Password + "@localhost:" + dbConfig.Port + "/" + dbConfig.Database)
-	if err != nil {
-		log.Fatal("Can not parse to PostgreSQL db")
-	}
-	db := pg.Connect(opt)
-
+func MakeConection(dbConfig config.DataBaseConfig) *bun.DB {
+	dsn := fmt.Sprintf("postgresql://%s:%s@localhost:%s/%s?sslmode=disabled", dbConfig.User, dbConfig.Password, dbConfig.Port, dbConfig.Database)
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	db := bun.NewDB(sqldb, pgdialect.New())
 	return db
 }
 
-func createSchema(db *pg.DB) {
+// func createSchema(db *pg.DB) {
 
-}
+// }
